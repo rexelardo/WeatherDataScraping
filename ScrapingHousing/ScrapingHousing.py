de52705d1394522e22906d5e5f072cf97dd9436b
Rex Elardo <rexelardo@gmail.com>
2021-09-13 17:14:47 -0400

    OK still some errors, this time every 180 hits

--- ScrapingHousing/ScrapingHousing.py
+++ ScrapingHousing/ScrapingHousing.py
@@ -27,6 +27,7 @@ import os
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options
 from selenium.common.exceptions import TimeoutException
 # import base64
 # import streamlit as st
@@ -53,13 +54,13 @@ home_data = {'city': city_scrape, 'state':state_scrape,'county':[], 'number of h
 for i, j in zip(home_data['city'],home_data['state']):
     url = f'https://www.bestplaces.net/housing/city/{j}/{i}'
     # trying the method to deploy on Heroku
-    chrome_options = webdriver.ChromeOptions()
+    chrome_options = Options()
     chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("disable-features=NetworkService") 
-    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
     driver.get(url)
     # This part will be to see where the Excel is stored
     dict_df = pd.DataFrame({ key:pd.Series(value) for key, value in home_data.items() })
