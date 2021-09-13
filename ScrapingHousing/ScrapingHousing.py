234658a4fc52aaacaa7d13ded5d49b0811bdbf49
Rex Elardo <rexelardo@gmail.com>
2021-09-13 16:11:39 -0400

    OK so now I can retrieve something from Heroku bash, but it just dies every 100 or so iterations. Let's see if this magic line of code fixes the issue

--- ScrapingHousing/ScrapingHousing.py
+++ ScrapingHousing/ScrapingHousing.py
@@ -53,11 +53,12 @@ home_data = {'city': city_scrape, 'state':state_scrape,'county':[], 'number of h
 for i, j in zip(home_data['city'],home_data['state']):
     url = f'https://www.bestplaces.net/housing/city/{j}/{i}'
     # trying the method to deploy on Heroku
-    chrome_options = webdriver.ChromeOptions()
+    chrome_options = webdriver.options()
     chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--no-sandbox")
+    option.addArguments("disable-features=NetworkService") 
     driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
     driver.get(url)
     # This part will be to see where the Excel is stored
