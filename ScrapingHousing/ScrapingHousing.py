dc6b870881fffd0300fc0a7d3493dd6ca498f802
Rex Elardo <rexelardo@gmail.com>
2021-09-13 19:26:34 -0400

    OK silly mistake

--- ScrapingHousing/ScrapingHousing.py
+++ ScrapingHousing/ScrapingHousing.py
@@ -59,7 +59,7 @@ for i, j in zip(home_data['city'],home_data['state']):
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--no-sandbox")
-    chrome_options.addArguments("enable-features=NetworkServiceInProcess")
+    chrome_options.add_argument("enable-features=NetworkServiceInProcess")
     driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
     
     # This part will be to see where the Excel is stored
