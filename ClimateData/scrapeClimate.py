25aad5f767201b01c15b9ee9effd95f299b09008
rex elardo <rexelardo@gmail.com>
2021-09-22 13:45:01 -0400

    OK needed to fix a quick thing before running the climate data scraper

--- ClimateData/scrapeClimate.py
+++ ClimateData/scrapeClimate.py
@@ -63,7 +63,7 @@ for i, j in zip(climate_data['city'],climate_data['state']):
     driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
     
     # This part will be to see where the Excel is stored
-    dict_df = pd.DataFrame({ key:pd.Series(value) for key, value in home_data.items() })
+    dict_df = pd.DataFrame({ key:pd.Series(value) for key, value in climate_data.items() })
     dict_df.to_csv('climateData.csv')
     
     try:    
