a2bbf080ab6c2dff6142aceabc4549edff4678ec
rex elardo <rexelardo@gmail.com>
2021-09-27 16:26:29 -0400

    Ok 10000 to go basically

--- ScrapingCostofLiving/scrapingCostofLiving.py
+++ ScrapingCostofLiving/scrapingCostofLiving.py
@@ -21,7 +21,7 @@ for i in cities_needed:
 
 
 
-cost_of_living_data = {'city': city_scrape[1311:], 'state':state_scrape[1311:],'county':[], 'Overall':[],'Grocery':[],'Health':[],\
+cost_of_living_data = {'city': city_scrape[4052:], 'state':state_scrape[4052:],'county':[], 'Overall':[],'Grocery':[],'Health':[],\
                        'Housing':[],\
                       'Median Home Cost':[], 'Utilities':[], 'Transportation':[], 'Miscellaneous':[]}
 
